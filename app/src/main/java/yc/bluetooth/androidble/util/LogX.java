ffa2cac8cd1a8835be0f22060e688428defdd31a
Yeungtinlong <752000307@qq.com>
2024-04-25 22:19:44 +0800

    Release Version

--- app/src/main/java/yc/bluetooth/androidble/util/LogX.java
+++ app/src/main/java/yc/bluetooth/androidble/util/LogX.java
@@ -3,30 +3,30 @@ package yc.bluetooth.androidble.util;
 import android.util.Log;
 
 public class LogX {
-    public static final boolean openLog = false;
+    public static final boolean openLog = true;
 
-    public final static void d(String tag, String message) {
+    public static void d(String tag, String message) {
         if (!openLog)
             return;
 
         Log.d(tag, message);
     }
 
-    public final static void e(String tag, String message) {
+    public static void e(String tag, String message) {
         if (!openLog)
             return;
 
         Log.e(tag, message);
     }
 
-    public final static void i(String tag, String message) {
+    public static void i(String tag, String message) {
         if (!openLog)
             return;
 
         Log.i(tag, message);
     }
 
-    public final static void w(String tag, String message) {
+    public static void w(String tag, String message) {
         if (!openLog)
             return;
 
